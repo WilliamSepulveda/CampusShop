d044566c95f6d2e723e0b86b3c8721587074a120
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-07-05 09:34:35 -0500

    feat :sparkles: importacion de la plantilla de imagenes

--- js/abrigos.js
+++ js/abrigos.js
@@ -5,55 +5,62 @@ let abrigos = async()=>{
     let abrigo = getAllropa();
     let abrigos = abrigos[0];
 
+    let contenedor1 = document.querySelector("#contenedor_pro"); 
+    let contenedor2 = document-querySelector("#contenedor_pro2");
+
+
     let plantilla = `
-        <div class="contenedor_products">
-        <div class="contenedor_article">
-            <img src="storage/image/camisa.png" alt="">
-            <div class="cont_price">
-            </div>
-        </div>
-        <div class="contenedor_article">
-            <img src="storage/image/camisa.png" alt="">
-
-            <div class="cont_price">
-            </div>
-        </div>
-        <div class="contenedor_article">
-            <img src="storage/image/camisa.png" alt="">
-
-            <div class="cont_price">
-            </div>
-        </div>
-        <div class="contenedor_article">
-            <img src="storage/image/camisa.png" alt="">
-
-            <div class="cont_price">
-            </div>
+    <div id="contenedor_pro" class="contenedor_products">
+    <div class="contenedor_article">
+        <img src="storage/image/camisa.png" alt="">
+        <div class="cont_price">
         </div>
     </div>
-    <div class="contenedor_products2">   
-        <div class="contenedor_article2">
-            <img src="storage/image/camisa.png" alt="">
-            <div class="cont_price">
-            </div>
-        </div>
-        <div class="contenedor_article2">
-            <img src="storage/image/camisa.png" alt="">
+    <div class="contenedor_article">
+        <img src="storage/image/camisa.png" alt="">
 
-            <div class="cont_price">
-            </div>
+        <div class="cont_price">
         </div>
-        <div class="contenedor_article2">
-            <img src="storage/image/camisa.png" alt="">
-            <div class="cont_price">
-            </div>
+    </div>
+    <div class="contenedor_article">
+        <img src="storage/image/camisa.png" alt="">
+
+        <div class="cont_price">
         </div>
-        <div class="contenedor_article2">
-            <img src="storage/image/camisa.png" alt="">
-            <div class="cont_price">
-            </div>
+    </div>
+    <div class="contenedor_article">
+        <img src="storage/image/camisa.png" alt="">
+
+        <div class="cont_price">
         </div>
+    </div>
     </div>
     `;
     return contenedor1 =  plantilla;
+    let plantilla2 = `
+    <div id="contenedor_pro2" class="contenedor_products2">   
+        <div class="contenedor_article2">
+            <img src="storage/image/camisa.png" alt="">
+            <div class="cont_price">
+            </div>
+        </div>
+        <div class="contenedor_article2">
+            <img src="storage/image/camisa.png" alt="">
+
+            <div class="cont_price">
+            </div>
+        </div>
+        <div class="contenedor_article2">
+            <img src="storage/image/camisa.png" alt="">
+            <div class="cont_price">
+            </div>
+        </div>
+        <div class="contenedor_article2">
+            <img src="storage/image/camisa.png" alt="">
+            <div class="cont_price">
+            </div>
+        </div>
+    </div>
+        `;
+        return contenedor2 =  plantilla;
 }
